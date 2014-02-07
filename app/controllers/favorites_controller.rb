aab5215883bf7a59aea1fa507662bf2edb767cd4
Cody Schaaf <cody@csc8.com>
2014-02-06 22:30:50 -0800

    woo hoo

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -2,8 +2,8 @@ class FavoritesController < ApplicationController
 	before_action :signed_in_user
 
 	def create 
-		puts "Time to create"
 		@movie = Movie.find(params[:favorite][:favorited_id])
+		@index = params[:index]
 		current_user.favorite!(@movie)
 		respond_to do |format|
 			format.html { redirect_to :back }
@@ -12,8 +12,8 @@ class FavoritesController < ApplicationController
 	end
 
 	def destroy
-		puts "Time to destroy"
 		@movie = Favorite.find(params[:id]).favorited
+		@index = params[:index]
 		current_user.unfavorite!(@movie)
 		respond_to do |format|
 			format.html { redirect_to :back }
